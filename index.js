85aa110868f78e4698963bf4ec6904732fc6ecc5
Pammmmaela <gutierrez.pamelaann2008@gmail.com>
2026-08-09 18:06:52 +0800

    08-09-26, 18:07

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let startButton = document.getElementById("startButton");
+let firstDiv = document.getElementById("firstDiv");
+let insideDiv = document.getElementById("insideDiv");
+
 window.addEventListener("load", () => {
     document.getElementById("dateTime").innerHTML = Date();
 });
@@ -6,3 +10,11 @@ document.addEventListener("click", () => {
     const bgm = document.getElementById("BGM");
     bgm.play();
 }, {once: true});
+
+startButton.addEventListener("click", () => {
+    firstDiv.classList.add("hidden");
+    insideDiv.classList.add("visible");
+
+    firstDiv.style.zIndex = 0;
+    insideDiv.style.zIndex = 1;
+})
