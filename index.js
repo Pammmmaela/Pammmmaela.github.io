44ebab3a4167651fcb910e686c5f7c208814d92e
Pammmmaela <gutierrez.pamelaann2008@gmail.com>
2026-08-09 20:19:51 +0800

    Added linear animation for one sud

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 let startButton = document.getElementById("startButton");
 let firstDiv = document.getElementById("firstDiv");
 let insideDiv = document.getElementById("insideDiv");
+let bubbleImg = document.getElementById("buble");
 
 window.addEventListener("load", () => {
     document.getElementById("dateTime").innerHTML = Date();
+
+    bubbleImg.style.top = "-6%";
 });
 
 document.addEventListener("click", () => {
@@ -27,3 +30,21 @@ startButton.addEventListener("click", () => {
 // bubleNew.style.left = tempoX
 // TIYAKA MGA IBA PANG PROPERTIES LIEK OPACITY 
 // transition top 94% to -6% ease
+
+function makeBubble() {
+    let temporaryX = Math.floor(Math.random() * 1100);
+    let bubleNew = document.createElement("img");
+
+    bubleNew.setAttribute("src", "assets/sud.png");
+
+    bubleNew.style.position = "absolute";
+    bubleNew.style.zIndex = -99;
+    bubleNew.style.height = "50px";
+    bubleNew.style.width = "50px";
+    bubleNew.style.opacity = 0.25;
+    bubleNew.style.left = temporaryX;
+    bubleNew.style.top = "94%";
+}
+
+/* 1100px 94% */
+/* 0px -6% */
